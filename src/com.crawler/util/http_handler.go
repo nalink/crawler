8891360bf8d92bdd3401aa51f7d6ead348c46e66
Nalin Kumar <nalin.kumar12@gmail.com>
2019-10-30 16:54:20 +0530

    Add files via upload

--- src/com.crawler/util/http_handler.go
+++ src/com.crawler/util/http_handler.go
@@ -1,1 +1,36 @@
+package util
 
+import (
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+const (
+	HTTP = "http"
+)
+
+func Href(tk html.Token) (string, bool) {
+	for _, attr := range tk.Attr {
+		if attr.Key == "href" {
+			return attr.Val, true
+		}
+	}
+
+	return "", false
+}
+
+func IsValidDomain(url string, domains []string) bool {
+	for _, domain := range domains {
+		// true if starts with any valid domain
+		if strings.Index(url, domain) == 0 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsHttpUrl(url string) bool {
+	return strings.Index(url, HTTP) == 0
+}
